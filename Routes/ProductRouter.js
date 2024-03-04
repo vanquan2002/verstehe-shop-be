2b6d1d0e01b650fdcc9fcf6672e6d6a5d8c48c24
Pham Van Quan <quan100202@gmail.com>
2024-03-04 18:52:20 +0700

    update

--- Routes/ProductRouter.js
+++ Routes/ProductRouter.js
@@ -8,8 +8,22 @@ const productRoute = express.Router();
 productRoute.get(
   "/",
   asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+    const pageSize = 3;
+    const page = Number(req.query.pageNumber) || 1;
+    const keyword = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword })
+      .limit(pageSize)
+      .skip(pageSize * (page - 1))
+      .sort({ _id: -1 });
+    res.json({ products, page, pages: Math.ceil(count / pageSize) });
   })
 );
 
